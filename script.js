e8baecd7761763b74e680039a050d27a6ade3f9e
Satpute Jyotiram <118312612+RSatpute341@users.noreply.github.com>
2022-11-16 03:21:38 +0530

    Level1_Task3

---
+++ script.js
@@ -0,0 +1,18 @@
+let celsius = document.getElementById("celsius");
+let fahrenheit = document.getElementById("fahrenheit");
+
+function celToFar() {
+  let output = (parseFloat(celsius.value) * 9 / 5) + 32;
+  fahrenheit.value = parseFloat(output.toFixed(4));
+}
+
+function farToCel() {
+  let output = (parseFloat(fahrenheit.value) - 32) * 5 / 9;
+  celsius.value = parseFloat(output.toFixed(4));
+  console.log(output);
+}
+
+function reset() {
+  document.getElementById("celsius").value = " ";
+  document.getElementById("fahrenheit").value = " ";
+}
